b9cb73aa49d4d3bad079d9e001116a64e79a0eba
federicatommasini <federica.tommasini@mail.polimi.it>
2023-05-04 16:04:30 +0200

    added calendar

--- app/src/main/java/com/polimi/dima/uniquizapp/composables/BottomNavContentScreens.kt
+++ app/src/main/java/com/polimi/dima/uniquizapp/composables/BottomNavContentScreens.kt
@@ -4,23 +4,17 @@ import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
-import com.polimi.dima.uniquizapp.BottomNavItem
 import com.polimi.dima.uniquizapp.BottomNavigationBar
-import com.polimi.dima.uniquizapp.R
 import com.polimi.dima.uniquizapp.ui.theme.*
 
 
