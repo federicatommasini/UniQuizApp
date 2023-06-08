6ba56233c15c45e183a936a71c5362888639931b
federicatommasini <federica.tommasini@mail.polimi.it>
2023-06-08 17:46:16 +0200

    first call to backend

--- app/src/main/java/com/polimi/dima/uniquizapp/MainViewModelFactory.kt
+++ app/src/main/java/com/polimi/dima/uniquizapp/MainViewModelFactory.kt
@@ -2,7 +2,6 @@ package com.polimi.dima.uniquizapp
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import com.polimi.dima.uniquizapp.data.repository.Repository
 import com.polimi.dima.uniquizapp.data.repository.UserRepository
 
 
