4e947d041dd816991e5e5e167590db827a675b03
giuliadauria <giulia.dauria@mail.polimi.it>
2023-06-20 18:44:39 +0200

    Ugly dropdown menu of universities sign up

---
+++ app/src/main/java/com/polimi/dima/uniquizapp/ui/composables/DropDownTextField.kt
@@ -0,0 +1,87 @@
+package com.polimi.dima.uniquizapp.ui.composables
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.DropdownMenu
+import androidx.compose.material.DropdownMenuItem
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.ExposedDropdownMenuBox
+import androidx.compose.material.Icon
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Text
+import androidx.compose.material.TextFieldDefaults
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material3.AlertDialogDefaults.shape
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import com.polimi.dima.uniquizapp.ui.theme.grayBackground
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun DropDownTextField(
+    items: List<String>,
+    selectedItem: String,
+    onItemSelected: (String) -> Unit
+) {
+    var expanded by remember { mutableStateOf(false) }
+    var textFieldValue by remember { mutableStateOf(selectedItem) }
+
+    ExposedDropdownMenuBox(expanded = expanded, onExpandedChange = {expanded = !expanded }) {
+
+
+        OutlinedTextField(
+            value = textFieldValue,
+            onValueChange = {
+                textFieldValue = it
+            },
+            label = { Text(text = "University") },
+            placeholder = {Text(text = "University")},
+
+            //readOnly = true,
+            colors = TextFieldDefaults.textFieldColors(
+                unfocusedIndicatorColor = Color.Transparent),
+            shape = RoundedCornerShape(20.dp),
+            modifier = Modifier
+                .fillMaxWidth(0.8f)
+                .background(grayBackground, RoundedCornerShape(20.dp))
+                .clickable(onClick = { expanded = true }),
+            trailingIcon = {
+                Icon(
+                    imageVector = Icons.Default.ArrowDropDown,
+                    contentDescription = "Dropdown Icon",
+                    modifier = Modifier.clickable { expanded = true }
+                )
+            }
+        )
+
+
+        ExposedDropdownMenu(
+            expanded = expanded,
+            onDismissRequest = { expanded = false },
+            modifier = Modifier.fillMaxWidth()
+                .background(grayBackground, RoundedCornerShape(20.dp))
+        ) {
+            items.forEach { item ->
+                DropdownMenuItem(
+                    onClick = {
+                        onItemSelected(item)
+                        textFieldValue = item
+                        expanded = false
+                    }
+                ) {
+                    Text(text = item)
+                }
+            }
+        }
+    }
+
+}
